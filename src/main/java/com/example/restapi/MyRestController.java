a0374e0b440d1dd5188cfdb570d91b57aae2b07e
matthijs Dolmans <487249@student.fontys.nl>
2023-04-06 13:46:13 +0200

    api with gyms

--- src/main/java/com/example/restapi/MyRestController.java
+++ src/main/java/com/example/restapi/MyRestController.java
@@ -3,13 +3,14 @@ package com.example.restapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
 @RestController
 public class MyRestController {
 
     @GetMapping("/hello")
-    @CrossOrigin(origins = "http://localhost:5173")
-    public String getHello() {
-        String[] messages = {"Hello", "World", "from Spring Boot!"};
-        return "{ \"message1\": \"" + messages[0] + "\", \"message2\": \"" + messages[1] + "\", \"message3\": \"" + messages[2] + "\" }";
+    @CrossOrigin(origins = "http://localhost:5174")
+    public String[] getHello() {
+        String[] messages = {"Breda", "Eindhoven", "Denbosch"};
+        return messages;
     }
 }
