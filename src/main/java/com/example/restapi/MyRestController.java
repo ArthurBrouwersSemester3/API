4e07426cba50a08a79841d9024477fdbdf123da5
matthijs Dolmans <487249@student.fontys.nl>
2023-04-06 11:59:48 +0200

    api to front end

--- src/main/java/com/example/restapi/MyRestController.java
+++ src/main/java/com/example/restapi/MyRestController.java
@@ -2,12 +2,14 @@ package com.example.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 public class MyRestController {
 
     @GetMapping("/hello")
+    @CrossOrigin(origins = "http://localhost:5173")
     public String getHello() {
-        return "matta is slecht!";
+        String[] messages = {"Hello", "World", "from Spring Boot!"};
+        return "{ \"message1\": \"" + messages[0] + "\", \"message2\": \"" + messages[1] + "\", \"message3\": \"" + messages[2] + "\" }";
     }
 }
