1c38ab5fcde3a933e93e5b5b16cb716eb1eb2cfb
matthijs Dolmans <487249@student.fontys.nl>
2023-04-13 09:21:25 +0200

    api update

---
+++ src/main/java/Data.java
@@ -0,0 +1,16 @@
+public class Data {
+    private String time;
+    private String day;
+    private int value;
+
+    // constructor
+    public Data(String time, String day, int value) {
+        this.time = time;
+        this.day = day;
+        this.value = value;
+    }
+
+    // getters and setters
+    // ...
+}
+
--- src/main/java/com/example/restapi/MyRestController.java
+++ src/main/java/com/example/restapi/MyRestController.java
@@ -3,14 +3,71 @@ package com.example.restapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import java.util.ArrayList;
 @RestController
 public class MyRestController {
 
     @GetMapping("/hello")
     @CrossOrigin(origins = "http://localhost:5174")
-    public String[] getHello() {
+    public String[] GetGyms() {
         String[] messages = {"Breda", "Eindhoven", "Denbosch"};
         return messages;
     }
+
+
+    @GetMapping("/hello1")
+    @CrossOrigin(origins = "http://localhost:5174/")
+    public List<MyData> get() {
+        List<MyData> list = new ArrayList<>();
+
+        for (int i = 0; i < 13; i++) {
+            int finalI = i;
+            var tempdata = new MyData() {
+                {
+                    setTime(8 + finalI + ":00");
+                    setDay("1");
+                    setValue(30);
+                }
+            };
+            list.add(tempdata);
+        }
+        return list;
+    }
+
+    class MyData {
+        @JsonProperty("time")
+        private String time;
+        @JsonProperty("day")
+        private String day;
+        @JsonProperty("value")
+        private int value;
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+        public String getDay() {
+            return day;
+        }
+
+        public void setDay(String day) {
+            this.day = day;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
 }
+
+
