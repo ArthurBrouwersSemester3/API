85e505e44548dceaf7fb5d0ec165fe08ae869508
matthijs Dolmans <487249@student.fontys.nl>
2023-06-08 11:05:25 +0200

    structure/name

--- src/main/java/com/example/restapi/controllers/GymController.java
+++ src/main/java/com/example/restapi/controllers/GymController.java
@@ -2,12 +2,13 @@ package com.example.restapi.controllers;
 
 import com.example.restapi.DAL.GymRepository;
 import com.example.restapi.classes.Gym;
+import com.example.restapi.classes.MyData;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 import java.util.ArrayList;
 @RestController
@@ -34,12 +35,11 @@ public class GymController {
     }
 
 
-
     @GetMapping("/graphdata")
     @CrossOrigin(origins = "http://localhost:5174")
     public List<MyData> get(@RequestParam String gym) {
         List<MyData> list = new ArrayList<>();
-        if(gym.equalsIgnoreCase("Eindhoven")){
+        if (gym.equalsIgnoreCase("Eindhoven")) {
             for (int i = 0; i < 13; i++) {
                 int finalI = i;
                 var tempdata = new MyData() {
@@ -54,40 +54,9 @@ public class GymController {
         }
         return list;
     }
+}
 ///
 
-    class MyData {
-        @JsonProperty("time")
-        private String time;
-        @JsonProperty("day")
-        private String day;
-        @JsonProperty("value")
-        private int value;
 
-        public String getTime() {
-            return time;
-        }
-
-        public void setTime(String time) {
-            this.time = time;
-        }
-
-        public String getDay() {
-            return day;
-        }
-
-        public void setDay(String day) {
-            this.day = day;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-    }
-}
 
 
